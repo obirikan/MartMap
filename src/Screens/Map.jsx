b0f17382706025139039e15eb838a31fa85ce62a
obirikan <obirikan020@gmail.com>
2023-01-30 23:57:50 +0000

    modules++

---
+++ src/Screens/Map.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Map = () => {
+  return (
+    <SafeAreaView>
+      <Text>Map</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Map
+
+const styles = StyleSheet.create({})

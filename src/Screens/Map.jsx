d31f2c52a73f06710ce40bfb7c1c6498be7c6fb6
kelvin obiri <obirikan020@gmail.com>
2023-02-05 16:48:59 +0000

    Kay (#5)
    * modules++
    
    * modules++
    
    * mapped

--- src/Screens/Map.jsx
+++ src/Screens/Map.jsx
@@ -6,15 +6,32 @@ import { Ionicons,FontAwesome5,MaterialCommunityIcons,Foundation,AntDesign } fro
 
 const Map = () => {
     const mapRef = useRef();
-    const svgMarker = {
-        path: "M-1.547 12l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-        fillColor: "blue",
-        fillOpacity: 0.6,
-        strokeWeight: 0,
-        rotation: 0,
-        scale: 2,
-      };
+    const data=[
+      {
+        latitude : 19.7545620,
+        longitude : 96.203399,
+      },
+      {
+        latitude : 19.7545620,
+        longitude : 96.202292,
+      }, 
+      {
+        latitude : 18.7545620,
+        longitude : 96.201392,
+      },
+      {
+        latitude : 19.7245621,
+        longitude : 96.203391,
+      },
+      {
+        latitude : 18.7545620,
+        longitude : 96.403392,
+      },
+
+    ]
+
   return (
+    
     <MapView
     ref={mapRef}
     style={{flex:1}}
@@ -23,6 +40,7 @@ const Map = () => {
     showsPointsOfInterest={false}
     showsScale={false}
   >
+    {data.map((dat)=>(
       <Marker
           draggable
        style = {{ width:60, height:40 }}
@@ -38,6 +56,7 @@ const Map = () => {
       >
                                             <FontAwesome5 name="store" size={34} color="blue" />
       </Marker>
+
   </MapView>
   )
 }
